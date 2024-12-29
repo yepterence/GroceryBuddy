d83cdbc7745d920404914ac77212bede1a413e26
Terence Yep <1744219-yepterence@users.noreply.gitlab.com>
2024-12-28 21:37:08 -0500

    Added checkbox and list item element creation when enter is pressed.

--- src/components/CreateItemList.tsx
+++ src/components/CreateItemList.tsx
@@ -1,39 +1,66 @@
 import { useState } from "react";
 
 const CreateItemList = () => {
+  const [title, setTitle] = useState("");
   const [newItem, setNewItem] = useState("");
   const [items, setItems] = useState([]);
-  const [newTitle, setNewTitle] = useState("");
+
   const handleTitleChange = (e) => {
-    setNewTitle(e.target.value);
+    setTitle(e.target.value);
   };
+
   const handleNewItemChange = (e) => {
     setNewItem(e.target.value);
   };
-  const handleAddItem = (e) => {
-    e.preventDefault();
-    if (newItem.trim() !== "") {
-      setItems([...items, newItem]);
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter" && newItem.trim() !== "") {
+      e.preventDefault();
+      setItems([...items, { name: newItem, checked: false }]);
       setNewItem("");
     }
   };
+
+  const toggleItemChecked = (index) => {
+    setItems(
+      items.map((item, i) =>
+        i === index ? { ...item, checked: !item.checked } : item
+      )
+    );
+  };
+
   return (
     <div>
-      <form onSubmit={handleAddItem}>
+      <form onSubmit={(e) => e.preventDefault()}>
         <input
           type="text"
           name="title"
           placeholder="Title"
+          value={title}
           onChange={handleTitleChange}
         />
-
+        {items.map((item, index) => (
+          <li key={index} className={item.checked ? "checked" : ""}>
+            <input
+              type="checkbox"
+              checked={item.checked}
+              className="input-list-checkbox"
+              onChange={() => toggleItemChecked(index)}
+            />
+            {item.name}
+          </li>
+        ))}
         <input
           type="text"
           placeholder="New Item"
-          onChange={handleNewItemChange}
           value={newItem}
+          onChange={handleNewItemChange}
+          onKeyDown={handleKeyDown}
         />
       </form>
+      <div>
+        <h3>{title}</h3>
+      </div>
     </div>
   );
 };
