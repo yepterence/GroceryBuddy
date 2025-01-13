3a752d4c020c5a77df83838015ca0a0c8f55a7a6
Terence Yep <1744219-yepterence@users.noreply.gitlab.com>
2025-01-12 23:12:56 -0500

    Added GroceryDashboard changes to map over lists derived from app store props

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import "./styles/theme.css";
 import Header from "./components/Header";
 import CreateItemList from "./components/CreateItemList";
 import GroceryDashboard from "./components/GroceryDashboard";
+import { useListStore } from "./store/applicationStore";
 
 export const App = () => {
+  const { groceryLists } = useListStore();
+  console.log({ groceryLists });
   return (
     <>
       <Header />
       <CreateItemList />
-      <GroceryDashboard />
+      <GroceryDashboard lists={groceryLists} />
     </>
   );
 };
--- src/components/GroceryDashboard.tsx
+++ src/components/GroceryDashboard.tsx
@@ -1,5 +1,24 @@
-const GroceryDashboard = () => {
-  return <div className="lists">Grocery Item Lists</div>;
+const GroceryDashboard = ({ lists }) => {
+  return (
+    <div className="lists">
+      {lists &&
+        lists.map((item) => (
+          <>
+            <h3>{item.title}</h3>
+            <div key={item.id}>
+              <li className={item.checked ? "list-item-checked" : "list-item"}>
+                <input
+                  type="checkbox"
+                  checked={item.checked}
+                  className="input-list-checkbox"
+                />
+                {item.name}
+              </li>
+            </div>
+          </>
+        ))}
+    </div>
+  );
 };
 
 export default GroceryDashboard;
