066063bb49ab255cba47ac1bcbdda320167bc6cc
Terence Yep <1744219-yepterence@users.noreply.gitlab.com>
2025-01-20 21:52:37 -0500

    Added endpoints to get flyer data

--- api/api.py
+++ api/api.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python3
+
 from sanic import Sanic
 from sanic import Blueprint
 from sanic.response import json
+from scraper import Scraper
 
 bp = Blueprint("lists_blueprint")
 app = Sanic("grocery_buddy_server")
@@ -9,6 +12,12 @@ app = Sanic("grocery_buddy_server")
 @bp.route("/")
 async def bp_root(request):
     return json({"server_name": "grocery_buddy_server"})
+
+
+@bp.route("/get-flyers")
+async def bp_get_flyers(request):
+    scraped_flyer = Scraper("")
+    print(scraped_flyer)
 
 
 app.blueprint(bp)
